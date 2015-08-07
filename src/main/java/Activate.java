75cac6842443e5501b9fff341781ccb8b5ea7831
seth-list <seth_t2@mail.ru>
2015-08-07 18:47:08 +0600

    main class for Project

---
+++ src/main/java/Activate.java
@@ -0,0 +1,34 @@
+import javax.swing.*;
+
+/**
+ * Class Activate - set Look and Feel
+ * Create GitForm
+ * @author seth-list
+ * @see GitForm
+ */
+public class Activate
+{
+    public static void main(String[] args)
+    {
+
+
+        //установка стиля приложения
+        try
+        {
+            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels())
+            {
+                if ("Nimbus".equals(info.getName()))
+                {
+                    UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            // If Nimbus is not available, you can set the GUI to another look and feel.
+        }
+
+        new GitForm();
+    }
+}
